c7c1d1def806c77d6b79e0aa822112eed396d746
Tarik Kaya <clickers331@gmail.com>
2022-02-01 14:08:15 +0300

    Added the timer functionality to the timer-display component

--- src/components/TimerDisplay.jsx
+++ src/components/TimerDisplay.jsx
@@ -1,29 +1,55 @@
-import React, { useContext, useState } from "react"
-import { TimeContext } from "../context/timeContext"
+import React, { useContext, useEffect, useState } from "react";
+import { TimeContext } from "../context/timeContext";
 
 function TimerDisplay() {
-  const timeContext = useContext(TimeContext) //Grab the timeContext to use in component
-  const [time, setTime] = useState(
-    timeContext.timeModeData.timeMode.timeInSeconds
-  ) //Create a copy of the timeInSeconds to reduce in component
-  const [timerGoing, setTimerGoing] = useState(false) //Create a timerGoing state to switch timer on and off
+  const timeContext = useContext(TimeContext); //Grab the timeContext to use in component
+  const [time, setTime] = useState(10); //Create a copy of the timeInSeconds to reduce in component
+  const [timerGoing, setTimerGoing] = useState(false); //Create a timerGoing state to switch timer on and off
 
-  // Format minutes and seconds and stitch them together
+  // Format time into minutes and seconds and stitch them together
   const minutes =
-    (time - (time % 60)) / 60 < 1 ? "" : (time - (time % 60)) / 60 + ":"
+    (time - (time % 60)) / 60 < 1 ? "" : (time - (time % 60)) / 60 + ":";
   const seconds = minutes
     ? time % 60 < 10
       ? `0${time % 60}`
       : time % 60
-    : time % 60
+    : time % 60;
 
-  const formattedTime = `${minutes}${seconds}`
+  const formattedTime = `${minutes}${seconds}`;
+
+  useEffect(() => {
+    console.log(time);
+    let timerTimeout;
+    const clearTimer = () => {
+      clearTimeout(timerTimeout);
+    };
+    if (timerGoing && time > 0) {
+      timerTimeout = setTimeout(() => {
+        setTime((prevTime) => prevTime - 1);
+      }, 1000);
+    } else {
+      clearTimer();
+      setTimerGoing(false);
+    }
+    return clearTimer;
+  }, [timerGoing, time]);
   return (
     <div>
       <p className="timer-display">{formattedTime}</p>
-      <button>Start/Stop</button>
+      <button
+        onClick={() => {
+          if (time === 0) {
+            setTime(10);
+            setTimerGoing((prevTimerGoing) => !prevTimerGoing);
+          } else {
+            setTimerGoing((prevTimerGoing) => !prevTimerGoing);
+          }
+        }}
+      >
+        {timerGoing ? "Stop" : "Start"}
+      </button>
     </div>
-  )
+  );
 }
 
-export default TimerDisplay
+export default TimerDisplay;
